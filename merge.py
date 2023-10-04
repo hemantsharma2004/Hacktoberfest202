abdb67e1e572b3cb861be0caabdd5dba90f54c93
hemantsharma2004 <125672327+hemantsharma2004@users.noreply.github.com>
2023-10-04 21:01:31 +0530

    Create merge.py
    mid search using binary search

---
+++ merge.py
@@ -0,0 +1,21 @@
+
+
+int binarySearch(int arr[], int low, int high, int x)
+{
+	while (low <= high) {
+
+		int mid = (low + high) / 2;
+
+		if (arr[mid] == x)
+			return mid;
+
+		if (arr[mid] <= x)
+			low = mid + 1;
+
+		else
+			high = mid - 1;
+	}
+
+	
+	return -1;
+}
